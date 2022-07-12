ded4a703543535486ec3eb1b245a08df97b9bab5
cuongnk <cuongnk@thinhtientech.com>
2022-07-12 09:37:06 +0700

    Refactoring

---
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Student;
+import com.example.demo.services.StudentService;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+  private final StudentService studentService;
+
+  @Autowired
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
+
+  @GetMapping
+  public List<Student> getStudents() {
+    return studentService.getStudents();
+  }
+
+  @GetMapping(path = "{studentId}")
+  public Optional<Student> getStudentById(
+      @PathVariable("studentId") Long studentId) {
+    return studentService.getStudentById(studentId);
+  }
+
+  @PostMapping
+  public Student registerNewStudent(@RequestBody Student student) {
+    return studentService.addNewStudent(student);
+  }
+
+  @DeleteMapping(path = "{studentId}")
+  public String deleteStudent(
+      @PathVariable("studentId") Long studentId) {
+    return studentService.deleteStudent(studentId);
+  }
+
+  @PutMapping(path = "{studentId}")
+  public Student updateStudent(
+      @PathVariable("studentId") Long studentId,
+      @RequestParam(required = false) String name,
+      @RequestParam(required = false) String email) {
+    return studentService.updateStudent(studentId, name, email);
+  }
+}
---
+++ src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository
+    extends JpaRepository<Student, Long> {
+
+  Optional<Student> findStudentById(Long studentId);
+
+  Optional<Student> findStudentByEmail(String email);
+}
